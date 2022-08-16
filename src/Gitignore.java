f865aec6e317892dcad8e6945b33312af75cb1a8
Omer Fatih ORUCU <omerfatihorucu@gmail.com>
2022-08-16 13:55:10 -0600

    V2-Branch

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -1,6 +1,8 @@
 public class Gitignore {
     public static void main(String[] args) {
         System.out.println("Gitignore Deneme 1");
+        System.out.println("Gitignore Deneme Branch");
+
 
     }
 }
