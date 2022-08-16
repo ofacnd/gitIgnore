4ca119387d576034c3b390b209d295bfb645caad
Omer Fatih ORUCU <omerfatihorucu@gmail.com>
2022-08-16 13:59:33 -0600

    Conflict Denemesi

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -2,6 +2,7 @@ public class Gitignore {
     public static void main(String[] args) {
         System.out.println("Gitignore Deneme 1");
         System.out.println("Gitignore Deneme Branch");
+        System.out.println("Gitignore Conflict Denemesi");
 
 
     }
